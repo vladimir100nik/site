b5bc81756cc96e08350fc91f13cd67b352e7e79f
vladimir100nik <42648313+vladimir100nik@users.noreply.github.com>
2020-11-02 14:50:27 +0200

    02.11.2020

--- assets/js/sparkle.js
+++ assets/js/sparkle.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".menu-btn").click(function(){
+    $(".menu-btn").click(function () {
         $(".menu-overlay").fadeIn(300);
         $("#menu").delay(320).css("right", "0");
     });
 
-    $(".menu-overlay").click(function(){
+    $(".menu-overlay").click(function () {
         $("#menu").css("right", "-101%");
         $(".menu-overlay").fadeOut(300);
     });
@@ -13,14 +13,34 @@ $(document).ready(function(){
     $(document).scroll(function () {
         if ($(this).scrollTop() > 500) {
             $(".scroll-to-top").fadeIn(500);
-        }
-        else{
+        } else {
             $(".scroll-to-top").fadeOut(500);
         }
     });
 
-    $(".scroll-to-top").click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 400);
+    $(".scroll-to-top").click(function () {
+        $("html, body").animate({scrollTop: 0}, 400);
     });
 });
 
+
+function block() {
+
+
+    let wrap = $("<div class='block'>")
+    for (let i = 0; i < 15; i++) {
+        let c = wrap.clone()
+        $("<span>").text(i).appendTo(c)
+        c.appendTo($("#gallery"));
+    }
+}
+block();
+
+// let map;
+//
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: 56.934556, lng: 24.077226},
+//         zoom: 12
+//     });
+// }
